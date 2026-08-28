e33df709c02d5ec1288234343b784cd5eddb1fe9
Lexyfirenze <your-email@example.com>
2026-08-28 23:47:07 +0100

    Point supabaseClient at choir-konnect Supabase project

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://qspczpunpgurhiitcuin.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFzcGN6cHVucGd1cmhpaXRjdWluIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODU0NzU2MzQsImV4cCI6MjEwMTA1MTYzNH0.vq5k1Tl2Bg7NTl-_drMBJxTeMFWP6jfzcSF5g0i5uFk";
+const supabaseUrl = "https://wjzyywzlxvucaunpmlic.supabase.co";
+const supabaseAnonKey =
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indqenl5d3pseHZ1Y2F1bnBtbGljIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODc5MjkwODksImV4cCI6MjEwMzUwNTA4OX0.Z0er5bEJzdkGrNJFM48yfYlI3s4-qR-A7umnKvGr61s";
 
-export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
